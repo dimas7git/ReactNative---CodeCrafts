b04b2df7d8a4cac95702167821f6770907c87912
Dimas F <dimaslimaferreira@gmail.com>
2024-02-27 10:53:25 -0300

    componente de classe estado

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -2,14 +2,31 @@ import React from 'react'
 import { Text, TextInput, Button } from 'react-native'
 import Estilo from '../estilo'
 
-export default class Mega extends React.Component{
+export default class Mega extends React.Component {
 
-    render(){
-        return(
-            <Text style={Estilo.fontG}>
-            Mega Sena
-            {this.props.qtdeNumeros}
-            </Text>
+    state = {
+        qtdeNumeros: this.props.qtdeNumeros,
+    }
+
+    alterarQtdeNumero = (qtde) => {
+        this.setState({ qtdeNumeros: qtde })
+    }
+
+    render() {
+        return (
+            <>
+                <Text style={Estilo.fontG}>
+                    Mega Sena
+                    {this.props.qtdeNumeros}
+                </Text>
+                <TextInput 
+                    keyboardType={'numeric'}
+                    style={{borderBottomWidth: 1}}    
+                    placeholder='Qtde de Números'
+                    value={`${this.state.qtdeNumeros}`}
+                    onChageText={this.alterarQtdeNumero}
+                />
+            </>
         )
     }
 }
