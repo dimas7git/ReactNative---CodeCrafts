46c5cb2831b679a5bbe85bbf2bcc7946e8ff2061
Dimas F <dimaslimaferreira@gmail.com>
2024-03-04 19:10:18 -0300

    projeto campo minado

---
+++ mines/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Iniciando o Mines!</Text>
+      <Text style={styles.welcome}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
