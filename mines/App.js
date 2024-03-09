0e1104f1c6daa7fb0beaa25b199ab2b9d9bc35cc
Dimas F <dimaslimaferreira@gmail.com>
2024-03-09 10:17:46 -0300

    componente campo #02

--- mines/App.js
+++ mines/App.js
@@ -8,6 +8,11 @@ export default function App() {
       <Text style={styles.welcome}>Iniciando o Mines!</Text>
       <Text style={styles.welcome}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
       <Field />
+      <Field opened />
+      <Field opened nearMines={1} />
+      <Field opened nearMines={2} />
+      <Field opened nearMines={3} />
+      <Field opened nearMines={6} />
     </View>
   );
 }
