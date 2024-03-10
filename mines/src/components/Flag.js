3a64b6d46d419a558800e6b43233bb12a151e89d
Dimas F <dimaslimaferreira@gmail.com>
2024-03-10 19:40:17 -0300

    componente bandeira #02

--- mines/src/components/Flag.js
+++ mines/src/components/Flag.js
@@ -4,10 +4,10 @@ import { View, StyleSheet } from "react-native";
 export default (props) => {
     return (
         <View style={styles.container}>
-        <View style={styles.flagpole} />
-        <View style={styles.flag} />
-        <View style={styles.base1} />
-        <View style={styles.base2} />
+        <View style={[styles.flagpole, props.bigger ? styles.flagpoleBigger : null]} />
+        <View style={[styles.flag, props.bigger ? styles.flagBigger : null]} />
+        <View style={[styles.base1, props.bigger ? styles.base1Bigger : null]} />
+        <View style={[styles.base2, props.bigger ? styles.base2Bigger : null]} />
         </View>
     );
 }
