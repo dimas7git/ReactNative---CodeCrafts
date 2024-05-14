aaa95b17a6bb4982cfed6d4bc50f0094dc6676a0
Dimas F <dimaslimaferreira@gmail.com>
2024-05-14 16:04:54 -0300

    navegação stack #01

--- Exercicios/src/navigation_2/Stack.js
+++ Exercicios/src/navigation_2/Stack.js
@@ -4,8 +4,8 @@ import {createStackNavigator} from '@react-navigation/stack'
 const Stack = createStackNavigator()
 
 export default props => (
-    <Stack.Navigator initialRouteName="TelaA">
-        <Stack.Screen name="TelaA" component={TelaA}/>
+    <Stack.Navigator initialRouteName="TelaA" screenOptions={{headerShown: true}}>
+        <Stack.Screen name="TelaA" options={{title: 'Informações Iniciais'}} component={TelaA}/>
         <Stack.Screen name="TelaB" component={TelaB}/>
         <Stack.Screen name="TelaC" component={TelaC}/>
     </Stack.Navigator>
